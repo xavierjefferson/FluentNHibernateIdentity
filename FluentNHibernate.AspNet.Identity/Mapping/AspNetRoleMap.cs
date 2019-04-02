f70ef351d37fd2349f6f49844c5e7d5d637dc7ad
Xavier Jefferson <xavier.jefferson@gohfc.com>
2019-04-02 13:35:09 -0700

    Update package versions.  Force unique names with roles

--- FluentNHibernate.AspNet.Identity/Mapping/AspNetRoleMap.cs
+++ FluentNHibernate.AspNet.Identity/Mapping/AspNetRoleMap.cs
@@ -10,7 +10,7 @@ namespace FluentNHibernate.AspNet.Identity.Mapping
             Table("AspNetRole");
             LazyLoad();
             Id(i => i.Id).GeneratedBy.Assigned().Length(128).Not.Nullable();
-            Map(i => i.Name).Column("`Name`").Not.Nullable().Length(256);
+            Map(i => i.Name).Column("`Name`").Not.Nullable().Length(256).UniqueKey("a");
             HasMany(i => i.AspNetUserRoles).KeyColumn("RoleId").Cascade.All();
         }
     }
